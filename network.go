af43696430b96aa41683418deeccc483d3c509d7
Jeremy Rand <jeremyrand@airmail.cc>
2022-03-07 21:56:03 +0000

    Use JSON array for Seeder list
    More semantically correct, and allows more than 3 seeders per network.

--- network.go
+++ network.go
@@ -19,9 +19,7 @@ type JNetwork struct {
 	DNSName   string
 	TTL       uint32
 	InitialIP string
-	Seeder1   string
-	Seeder2   string
-	Seeder3   string
+	Seeders   []string
 }
 
 func createNetFile() {
@@ -37,9 +35,11 @@ func createNetFile() {
 		Name:      "SeederNet",
 		Desc:      "Description of SeederNet",
 		InitialIP: "",
-		Seeder1:   "seeder1.example.com",
-		Seeder2:   "seed1.bob.com",
-		Seeder3:   "seed2.example.com",
+		Seeders:   []string{
+			"seeder1.example.com",
+			"seed1.bob.com",
+			"seed2.example.com",
+		},
 	}
 
 	f, err := os.Create("dnsseeder.json")
@@ -107,10 +107,7 @@ func initNetwork(jnw JNetwork) (*dnsseeder, error) {
 	seeder.initialIP = jnw.InitialIP
 
 	// load the seeder dns
-	seeder.seeders = make([]string, 3)
-	seeder.seeders[0] = jnw.Seeder1
-	seeder.seeders[1] = jnw.Seeder2
-	seeder.seeders[2] = jnw.Seeder3
+	seeder.seeders = jnw.Seeders
 
 	// add some checks to the start & delay values to keep them sane
 	seeder.maxStart = []uint32{20, 20, 20, 30}
